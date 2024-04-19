904cea2c750357448f7e6e670add4ab24e5a0efc
Aplulu <aplulu.liv@gmail.com>
2024-04-20 07:16:28 +0900

    :white_check_mark: vitestを追加

---
+++ src/lib/signal.spec.ts
@@ -0,0 +1,35 @@
+import { convertSignalToFrequency } from './signal';
+import { it, describe, expect } from 'vitest';
+
+describe('convertSignalToFrequency', () => {
+  it('水色の周波数が正常に出力されること', () => {
+    const id = 52; // 水色
+    const expectedFrequencyArray = [18500, 18750, 19500, 19250, 19000, 19250, 19000, 18750];
+    const result = convertSignalToFrequency(id);
+    expect(result).toEqual(expectedFrequencyArray);
+  });
+
+  it('消灯の周波数が正常に出力されること', () => {
+    const id = 62; // 消灯
+    const expectedFrequencyArray = [18500, 18750, 19500, 19250, 19500, 19250, 19500, 18750];
+    const result = convertSignalToFrequency(id);
+    expect(result).toEqual(expectedFrequencyArray);
+  });
+
+  it('ピンクの周波数が正常に出力されること', () => {
+    const id = 124; // ピンク
+    const expectedFrequencyArray = [18500, 19250, 19500, 19250, 19500, 19250, 19000, 18750];
+    const result = convertSignalToFrequency(id);
+    expect(result).toEqual(expectedFrequencyArray);
+  });
+
+  it('IDが負の値の場合、エラーが発生すること', () => {
+    const id = -1;
+    expect(() => convertSignalToFrequency(id)).toThrow('Invalid signal id');
+  });
+
+  it('IDが2の7乗以上の場合、エラーが発生すること', () => {
+    const id = Math.pow(2, 7);
+    expect(() => convertSignalToFrequency(id)).toThrow('Invalid signal id');
+  });
+});
--- src/lib/signal.ts
+++ src/lib/signal.ts
@@ -1,35 +1,5 @@
 const SIGNAL_BIT = 7;
-const SIGNAL_DURATION = 0.125;
 const FREQUENCY_MAP: number[] = [18500, 18750, 19000, 19250, 19500];
-
-const audioContext = new AudioContext();
-
-/**
- * ミラクルハートライトの制御シグナルを再生する
- */
-export const playSignal = (id: number) => {
-  const signalFrequency = convertSignalToFrequency(id);
-
-  const gainNode = audioContext.createGain();
-  gainNode.gain.value = 0.1;
-  gainNode.connect(audioContext.destination);
-
-  const startTime = audioContext.currentTime;
-  signalFrequency.forEach((frequency, i) => {
-    const oscillator = audioContext.createOscillator();
-    oscillator.type = 'sine';
-
-    oscillator.frequency.value = frequency;
-
-    oscillator.connect(gainNode);
-    oscillator.onended = () => {
-      oscillator.disconnect();
-    };
-
-    oscillator.start(startTime + i * SIGNAL_DURATION);
-    oscillator.stop(startTime + (i + 1) * SIGNAL_DURATION);
-  });
-};
 
 export const convertSignalToFrequency = (id: number): number[] => {
   if (id < 0 || id >= Math.pow(2, SIGNAL_BIT)) {
